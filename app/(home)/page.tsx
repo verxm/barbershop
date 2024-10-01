e8fb625e27eca152cdcd06237d55d588247a2871
Davi Santos <davi.santos@zenvia.com>
2024-10-01 19:41:51 -0300

    feat: add footer component.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -41,7 +41,7 @@ export default async function Home() {
         </div>
       </div>
 
-      <div className="mt-6">
+      <div className="mt-6 mb-[4.5rem]">
         <h2 className="px-5 text-xs uppercase text-gray-400 font-bold mb-3">Populares</h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
