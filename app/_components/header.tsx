394104657c6ced2de25b599d230ebf2ed52c6f5c
Davi Santos <davisantos448@gmail.com>
2024-12-10 15:51:52 -0300

    feat: ensure semantic tags are used on footer and header.

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -10,26 +10,28 @@ import Link from "next/link";
 
 const Header = () => {
 
-    return ( 
-        <Card>
-            <CardContent className="p-5 justify-between flex items-center flex-row">
-                <Link href="/">
-                    <Image src="/logo.png" alt="FSW Barber" height={22} width={120}/>
-                </Link>
-                <Sheet>
-                    <SheetTrigger asChild>
-                        <Button variant="outline" size="icon" className="h-9 w-9">
-                            <MenuIcon size={18}/>
-                        </Button>
-                    </SheetTrigger>
+    return (
+        <header>
+            <Card>
+                <CardContent className="p-5 justify-between flex items-center flex-row">
+                    <Link href="/">
+                        <Image src="/logo.png" alt="FSW Barber" height={22} width={120} />
+                    </Link>
+                    <Sheet>
+                        <SheetTrigger asChild>
+                            <Button variant="outline" size="icon" className="h-9 w-9">
+                                <MenuIcon size={18} />
+                            </Button>
+                        </SheetTrigger>
 
-                    <SheetContent className="p-0">
-                        <SideMenu />
-                    </SheetContent>
-                </Sheet>
-            </CardContent>
-        </Card>
-     );
+                        <SheetContent className="p-0">
+                            <SideMenu />
+                        </SheetContent>
+                    </Sheet>
+                </CardContent>
+            </Card>
+        </header>
+    );
 }
- 
+
 export default Header;
